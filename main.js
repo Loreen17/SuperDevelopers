d3f3fbc943ace4adc0966aa02920c8825f3f2343
Loreen <Loreen.h382@gmail.com>
2019-03-19 15:14:27 +0200

    working on the form

---
+++ main.js
@@ -0,0 +1,10 @@
+/*var form = document.getElementById("form-id");
+
+document.getElementById("submit").addEventListener("click", function () {
+  form.submit();
+});
+*/
+
+function myFunction() {
+  document.getElementById("form-id").submit();
+}
